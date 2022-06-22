16a3b36f4c6f886ab15eb85b058c0d7047df02fa
Shobit Nair <t-shobitnair@microsoft.com>
2022-06-22 14:26:43 +0530

    used findall

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include<UIAutomation.h>
 #include<UIAutomationClient.h>
-#include<UIAutomationCore.h>
 #include<queue>
 #include<vector>
-#include <random>
 #include<atlbase.h>
+
+#include <random>
 using namespace std;
 
 class UIA {
 public:
 	IUIAutomation* aut;
 	IUIAutomationElement* windowElement;
+	IUIAutomationElement* focusedElement;
+	IUIAutomationElementArray* controls;
 	HRESULT hr;
 	queue<IUIAutomationElement*> q;
 	vector<IUIAutomationElement*> elements;
@@ -49,6 +51,7 @@ public:
 	}
 
 	void windowFromFocus() {
+		aut->GetFocusedElement(&focusedElement);
 		aut->GetFocusedElement(&windowElement);
 		IUIAutomationElement* root = NULL;
 		IUIAutomationTreeWalker* tree = NULL;
@@ -56,7 +59,7 @@ public:
 		aut->GetRootElement(&root);
 		aut->get_ControlViewWalker(&tree);
 
-		BSTR name; windowElement->get_CurrentName(&name);
+		
 		BSTR rname; root->get_CurrentName(&rname);
 
 		while(true) {
@@ -71,7 +74,9 @@ public:
 			tree->GetParentElement(windowElement, &windowElement);
 
 		}
+		BSTR name; focusedElement->get_CurrentName(&name);
 		BSTR wname; windowElement->get_CurrentName(&wname);
+		wcout << name << endl;
 		wcout << wname << endl;
 		cout << controlType(windowElement) << endl;
 	}
@@ -101,6 +106,9 @@ public:
 		}
 	}
 
+	void editControls() {
+
+	}
 };
 
 string random_string(std::size_t length)
@@ -128,23 +136,39 @@ void main() {
 		UIA* uia = new UIA;
 		uia->init();
 		uia->windowFromFocus();
-		uia->editControls_BFS();
-		string res = random_string(10);
-		CComBSTR temp(res.c_str());
-		BSTR val = temp.Detach();
-		for (int i = 0; i < uia->elements.size(); i++) {
-			wcout << i + 1 << " " << uia->name(uia->elements[i]) << endl;
-			IUIAutomationValuePattern* pat;
-			IUIAutomationLegacyIAccessiblePattern* leg;
-			uia->elements[i]->GetCurrentPatternAs(UIA_ValuePatternId, IID_PPV_ARGS(&pat));
-			//uia->elements[i]->GetCurrentPatternAs(UIA_LegacyIAccessiblePatternId, IID_PPV_ARGS(&leg));
-			if (pat) {
-				pat->SetValue(val);
-				pat->Release();
-			}
+		//uia->editControls_BFS();
+
+		IUIAutomationCondition* condition;
+		IUIAutomationElementArray* f;
+
+		uia->aut->CreatePropertyCondition(UIA_IsTextEditPatternAvailablePropertyId, uia->truevar(), &condition);
+		uia->windowElement->FindAll(TreeScope_Subtree, condition, &f);
+
+		int index = 0;
+		int length; f->get_Length(&length);
+
+		for (int i = 0; i < length; i++) {
+			IUIAutomationElement* cur;
+			f->GetElement(i, &cur);
+			wcout << ">> " << uia->name(cur) << " : " << uia->controlType(cur) << endl;
 		}
+		
+		//for (int i = 0; i < uia->elements.size(); i++) {
+		//	wcout << i + 1 << " " << uia->name(uia->elements[i]) << endl;
+		//	IUIAutomationValuePattern* pat;
+		//	IUIAutomationLegacyIAccessiblePattern* leg;
+		//	uia->elements[i]->GetCurrentPatternAs(UIA_ValuePatternId, IID_PPV_ARGS(&pat));
+		//	//uia->elements[i]->GetCurrentPatternAs(UIA_LegacyIAccessiblePatternId, IID_PPV_ARGS(&leg));
+		//	
+		//	string res = random_string(10);
+		//	CComBSTR temp(res.c_str());
+		//	BSTR val = temp.Detach();
+		//	if (pat) {
+		//		pat->SetValue(val);
+		//		pat->Release();
+		//	}
+		//}
 		uia->elements.clear();
-		cout << "random value added : " << res << endl;
 		cout << endl;
 		delete(uia);
 	}
